32408af967ee3c75f54e8a046b1385f285a347ee
G Roques <groques360@gmail.com>
2021-08-14 12:22:14 -0500

    Switch snake-case to camel-case.

--- cat-file.js
+++ cat-file.js
@@ -13,13 +13,13 @@ const path = require('path');
 const hash = process.argv[2];
 
 // First two digits of hash (e.g. 9d).
-const object_directory = hash.slice(0, 2);
+const objectDirectory = hash.slice(0, 2);
 
 // Last 38 digits of hash,
 // (e.g. aeafb9864cf43055ae93beb0afd6c7d144bfa4).
-const object_filename = hash.slice(2);
+const objectFilename = hash.slice(2);
 
-const objectPath = path.join('.git', 'objects', object_directory, object_filename);
+const objectPath = path.join('.git', 'objects', objectDirectory, objectFilename);
 const object = fs.readFileSync(objectPath);
 const inflated = zlib.inflateSync(object);
 const contents = inflated.toString('utf8');
