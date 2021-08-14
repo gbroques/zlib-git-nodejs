338dd409ec291706dbbc1ea04ea91ec5309a3d1b
G Roques <groques360@gmail.com>
2021-08-14 11:30:12 -0500

    Add missing semi-colons

--- hash-object.js
+++ hash-object.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const stdin_fd = 0;
-const content = fs.readFileSync(stdin_fd, 'utf-8')
+const content = fs.readFileSync(stdin_fd, 'utf-8');
 console.dir(content);
-const shasum = crypto.createHash('sha1')
+const shasum = crypto.createHash('sha1');
 shasum.update(content);
 console.log(shasum.digest('hex'));
